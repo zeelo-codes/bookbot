d3757456be4090b96ac0404fb16b34a4fea2ab1e
zeelo <t2000r@gmail.com>
2025-03-02 09:45:40 -0700

    done

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from stats import sorter
 #def asd(x):
     #return x + 1
 
+import sys
 
-def main():
-    file_path = "books/frankenstein.txt"
+def main(file_path):
+    #file_path = "books/frankenstein.txt"
     the_book_text = get_book_text(file_path)
     num_words = words_in_book(file_path)
     #lower_case_book_text = to_lower(the_book_text)
     num_chars = char_counter(the_book_text)
     sorted_num_chars = sorter(num_chars)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
@@ -26,4 +27,8 @@ def main():
     print("============= END ===============")
     return num_chars
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        main(sys.argv[1])
